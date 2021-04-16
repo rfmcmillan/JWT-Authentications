698b008785954bbf2d94320e85a91d0d7ac9092b
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-04-16 17:09:49 -0400

    built front end

--- test/models.spec.js
+++ test/models.spec.js
@@ -1,80 +1,10 @@
-const bcrypt = require('bcrypt');
+const {
+  models: { User },
+  syncAndSeed,
+} = require('../db');
 const jwt = require('jsonwebtoken');
-const Sequelize = require('sequelize');
-const { STRING } = Sequelize.DataTypes;
-//this prevents logging of the Sequelize commands
-const config = {
-  logging: false,
-};
-//this allows you to enter 'LOGGING=true npm run test:dev' to delete the above
-//config and log the different commands
-if (process.env.LOGGING) {
-  delete config.logging;
-}
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_db'
-);
-
-const User = db.define('user', {
-  username: STRING,
-  password: STRING,
-});
-
-User.addHook('beforeSave', async function (user) {
-  if (user._changed.has('password')) {
-    user.password = await bcrypt.hash(user.password, 10);
-  }
-});
-
-const syncAndSeed = async () => {
-  await db.sync({ force: true });
-  const credentials = [
-    { username: 'lucy', password: 'lucy_pw' },
-    { username: 'larry', password: 'larry_pw' },
-    { username: 'moe', password: 'moe_pw' },
-  ];
-  const [lucy, larry, moe] = await Promise.all(
-    credentials.map((credential) => User.create(credential))
-  );
-  return {
-    users: {
-      lucy,
-      larry,
-      moe,
-    },
-  };
-};
-
-User.authenticate = async function ({ username, password }) {
-  const user = await User.findOne({
-    where: { username },
-  });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    return jwt.sign({ id: user.id }, process.env.JWT);
-  }
-  const error = Error('bad credentials');
-  error.status = 401;
-  throw error;
-};
-
-User.byToken = async function (token) {
-  try {
-    const { id } = await jwt.verify(token, process.env.JWT);
-    const user = await User.findByPk(id);
-    if (user) {
-      return user;
-    }
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  } catch (err) {
-    const error = Error('bad credentials');
-    error.status = 401;
-    throw error;
-  }
-};
-
 const { expect } = require('chai');
+
 describe('Models', () => {
   let seed;
   beforeEach(async () => (seed = await syncAndSeed()));
---
+++ test/routes.spec.js
@@ -0,0 +1,55 @@
+const {
+  models: { User },
+  syncAndSeed,
+} = require('../db');
+const jwt = require('jsonwebtoken');
+const { expect } = require('chai');
+const app = require('supertest')(require('../app'));
+
+describe('Routes', () => {
+  let seed;
+  beforeEach(async () => (seed = await syncAndSeed()));
+  describe('seeded data', () => {
+    it('there are 3 users', () => {
+      expect(Object.keys(seed.users).length).to.equal(3);
+    });
+  });
+  describe('POST /api/auth', () => {
+    describe('with valid credentials', () => {
+      it('returns a token', async () => {
+        const response = await app
+          .post('/api/auth')
+          .send({ username: 'larry', password: 'larry_pw' });
+        expect(response.status).to.equal(200);
+        expect(response.body.token).to.be.ok;
+      });
+    });
+
+    describe('with invalid credentials', () => {
+      it('returns a token', async () => {
+        const response = await app
+          .post('/api/auth')
+          .send({ username: 'larry', password: 'larry' });
+        expect(response.status).to.equal(401);
+        expect(response.body.error).to.equal('bad credentials');
+        console.log(response.body);
+      });
+    });
+  });
+
+  describe('GET /api/auth', () => {
+    describe('with valid token', () => {
+      it('returns a user', async () => {
+        const token = await jwt.sign(
+          { id: seed.users.lucy.id },
+          process.env.JWT
+        );
+        const response = await app
+          .get('/api/auth')
+          .set({ authorization: token });
+        expect(response.status).to.equal(200);
+        expect(response.body.username).to.equal('lucy');
+      });
+    });
+  });
+});
